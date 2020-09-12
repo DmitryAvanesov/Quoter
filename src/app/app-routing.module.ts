88a2e0c02ca3a8e5e4b7445172d90ca5c9797c54
Dima <dima.avanesov@yandex.ru>
2020-09-12 12:52:30 +0700

    Added routing to detail page from quotes and dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { QuotesComponent } from './quotes/quotes.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'quotes', component: QuotesComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: QuoteDetailComponent },
 ];
 
 @NgModule({
