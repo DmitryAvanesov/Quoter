ea8c978fb40419e9989ebdd236cf1fdb48c8c54c
Dima <dima.avanesov@yandex.ru>
2020-09-13 19:16:22 +0700

    Added in-memory-web-api, requested quotes from a local mock DB

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
-  declarations: [AppComponent, QuotesComponent, QuoteDetailComponent, MessagesComponent, DashboardComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [
+    AppComponent,
+    QuotesComponent,
+    QuoteDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false,
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/quote.service.ts
+++ src/app/quote.service.ts
@@ -3,20 +3,30 @@ import { Quote } from './quote';
 import { QUOTES } from './mock-quotes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class QuoteService {
-  constructor(private messageService: MessageService) {}
+  constructor(
+    private messageService: MessageService,
+    private httpClient: HttpClient
+  ) {}
+
+  private quotesUrl = 'api/quotes';
 
   getQuotes(): Observable<Quote[]> {
-    this.messageService.add('Quotes received');
-    return of(QUOTES);
+    this.log('Quotes received');
+    return this.httpClient.get<Quote[]>(this.quotesUrl);
   }
 
   getQuote(id: number): Observable<Quote> {
-    this.messageService.add(`Quote #${id} received`);
+    this.log(`Quote #${id} received`);
     return of(QUOTES[id]);
+  }
+
+  private log(message: string): void {
+    this.messageService.add(message);
   }
 }
