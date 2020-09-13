af33011e2c7b05a85ab29a634698e7171af2a2f1
Dima <dima.avanesov@yandex.ru>
2020-09-13 20:35:18 +0700

    Getting separate quotes, logging and handling errors via pipes
    + some quote text styling

--- src/app/quote.service.ts
+++ src/app/quote.service.ts
@@ -4,6 +4,7 @@ import { QUOTES } from './mock-quotes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient } from '@angular/common/http';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -17,16 +18,30 @@ export class QuoteService {
   private quotesUrl = 'api/quotes';
 
   getQuotes(): Observable<Quote[]> {
-    this.log('Quotes received');
-    return this.httpClient.get<Quote[]>(this.quotesUrl);
+    return this.httpClient.get<Quote[]>(this.quotesUrl).pipe(
+      tap(() => this.log('Quotes received')),
+      catchError(this.handleError<Quote[]>('getQuotes', []))
+    );
   }
 
   getQuote(id: number): Observable<Quote> {
-    this.log(`Quote #${id} received`);
-    return of(QUOTES[id]);
+    const quoteUrl = `${this.quotesUrl}/${id}`;
+
+    return this.httpClient.get<Quote>(quoteUrl).pipe(
+      tap(() => this.log(`Quote #${id} received`)),
+      catchError(this.handleError<Quote>(`getQuote (#${id})`))
+    );
   }
 
   private log(message: string): void {
     this.messageService.add(message);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
   }
 }
